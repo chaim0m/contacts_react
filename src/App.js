bffcc0809c5a86ecf2840b616c0a0ccb4cf53eaf
Unknown <chaimfinson@gmail.com>
2020-01-25 22:10:25 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import citizen from './assets/citizen.svg'
-// import professional from './assets/professional.svg'
 import './App.css';
 import ContactList from './contacts/ContactsList.js'
 
+
+
 function App() {
   return (
-    <div className="App">
-       <ContactList/>
-    </div>
+      <div className="App">
+        <ContactList />
+      </div>
   );
 }
 
